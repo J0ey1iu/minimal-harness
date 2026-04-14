4aecaea978048e015b670cf2bb15e198ab6cd1dd
J0ey1iu <549323981@qq.com>
2026-04-14 22:23:48 +0800

    refactor: quote __all__ strings in agent module

--- src/minimal_harness/agent/__init__.py
+++ src/minimal_harness/agent/__init__.py
@@ -2,9 +2,9 @@ from .openai import OpenAIAgent
 from .protocol import Agent, InputContentConversionFunction
 
 __all__ = [
-    Agent,
-    InputContentConversionFunction,
-    OpenAIAgent,
+    "Agent",
+    "InputContentConversionFunction",
+    "OpenAIAgent",
 ]
 
 
